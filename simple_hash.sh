88476c7c6e798b14e5bb0ec822123bdf3b1e41ca
adam <ageodru@gmail.com>
2021-02-17 15:24:40 -0500

    simple hash

---
+++ simple_hash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# While the `md5sum` command exists on its own and performs
+# the same task, all this script does is add the most basic
+# of input interfaces, then format the output.
+
+echo -n "Enter the path of the file you wish to hash: "
+read INPUT
+
+DIGEST=$(md5sum "$INPUT" | awk -F" " '{print $1}')
+
+echo "Your MD5 hash: "
+echo "//////////////"
+sleep .5s
+echo "File: $INPUT | Hash: $DIGEST"
+echo " "
